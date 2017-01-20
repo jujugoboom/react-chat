8de4ff6f761371c1f982fd91451024c68330e58d
Justin Covell <jujugoboom@gmail.com>
2017-01-20 16:49:16 +0000

    Stuff

--- src/App.js
+++ src/App.js
@@ -15,14 +15,15 @@ class App extends Component {
       this.pages = {
         updateProfile: <UpdateProfile onFinish={this.handleUpdatedProfile} />,
         contacts: <Contacts />,
-        messageThread: <MessageThread />
+        messageThread: "messageThread"
       };
       if(firebase.auth().currentUser.displayName === null){
-        this.state = {currentPage: this.pages.updateProfile};
+        this.state = {currentPage: this.pages.updateProfile, error: ''};
       }
       else{
-        this.state = {currentPage: this.pages.contacts};
+        this.state = {currentPage: this.pages.contacts, error: ''};
       }
+      this.logout = this.logout.bind(this);
     }
     handleUpdatedProfile(){
       this.changePage(this.pages.contacts);
@@ -30,11 +31,25 @@ class App extends Component {
     changePage(page){
       this.setState({currentPage: page});
     }
+    logout(){
+      firebase.auth().signOut().catch((e) => {
+        this.setState({error: e});
+      });
+    }
     render() {
         var currentPage = this.state.currentPage;
+        var errorDiv;
+        if(this.state.error !== ''){
+          errorDiv = <div>{this.state.error.message}</div>
+        }
       return (
+        <div>
+        <div className="logout">
+          <button onClick={this.logout}>Log out</button>
+        </div>
         <div className="App">
           {currentPage}
+        </div>
         </div>
       );
     }
--- src/Contacts.js
+++ src/Contacts.js
@@ -5,19 +5,19 @@ class Contacts extends Component{
     constructor(props){
         super(props);
         this.state = {confirmedContacts: [], unconfirmedContacts: [], pendingContacts: []};
+        this.currentUser = firebase.auth().currentUser;
         this.database = firebase.database();
         this.contactDatabase = this.database.ref('contacts/' + this.currentUser.uid);
         this.userDatabase = this.database.ref('users/');
-        this.currentUser = firebase.auth().currentUser;
     }
     componentDidMount(){
-        this.contactDatabase.child("unconfirmed").on((snapshot) => {
+        this.contactDatabase.on('unconfirmed',(snapshot) => {
             this.setState({unconfirmedContacts: snapshot.val()});
         });
-        this.contactDatabase.child("pending").on((snapshot) => {
+        this.contactDatabase.on('pending', (snapshot) => {
             this.setState({pendingContacts: snapshot.val()});
         });
-        this.contactDatabase.child("confirmed").on((snapshot) => {
+        this.contactDatabase.on('confirmed', (snapshot) => {
             this.setState({confirmedContacts: snapshot.val()});
         });
     }
@@ -25,7 +25,10 @@ class Contacts extends Component{
         this.userDatabase.orderByKey('username').limitToFirst(10).startAt(currentSearch);
     }
     render(){
-        return(<div></div>)
+        return(<div>
+            Contacts
+            <div></div>
+            </div>);
     }
 }
 
